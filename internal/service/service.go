d0d0e54b174e476942d227ff3ee8063bb50d93cd
Daniel Jr <daniel.souza.lima@outlook.com>
2024-12-06 17:10:43 -0300

    feat: Created ionservice package, the internal service manager

---
+++ internal/service/service.go
@@ -0,0 +1,14 @@
+package ionservice
+
+type IService interface {
+	Status() ServiceStatus
+	Start() error
+	Stop()
+}
+
+type ServiceStatus int
+
+const (
+	Stopped ServiceStatus = iota
+	Running
+)
