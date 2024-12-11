a73b7977f3143cf446aae7008ae40cd25adaca9f
Daniel Jr <daniel.souza.lima@outlook.com>
2024-12-11 17:51:30 -0300

    tests: Added simple logging benchmark

---
+++ internal/benchmark/simple_logs_test.go
@@ -0,0 +1,28 @@
+package benchmark
+
+import (
+	"testing"
+
+	"github.com/IonicHealthUsa/ionlog/pkg/ionlog"
+)
+
+type fakkeWriter struct{}
+
+func (fakkeWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func BenchmarkSimpleLogs(b *testing.B) {
+	// Set the log attributes, and other configurations
+	ionlog.SetLogAttributes(
+		ionlog.WithTargets(fakkeWriter{}),
+	)
+
+	// Start the logger service
+	ionlog.Start()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ionlog.Info("This log level is: %v", "info")
+	}
+}
